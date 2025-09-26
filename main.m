b0104be894443e49ff0c05d2ee0d74c3e657f8d7
mirachew <mira@thechewfamily.com>
2025-09-25 23:56:45 -0400

    completed day 1

--- main.m
+++ main.m
@@ -1,32 +1,51 @@
 % Initial guess
 clear
 close all
-run_simulation(pi/4, 10)
 
 
-return;
-X = [3; 2; 1];  
+% ------------------- TESTING JACOBIAN FUNCTION --------------------------
+X0 = rand(3, 1);
 
-tol = 1e-8;      
-max_iter = 50;  
+[~, J_analytical] = test_function01(X0);
+J_numerical = approximate_jacobian(@test_function01, X0);
 
-for k = 1:max_iter
-    [f_val, J] = test_function01(X);
-    
-    % Update step
-    delta = J \ f_val;
-    X_new = X - delta;
-    
-    % Check convergence
-    if norm(f_val) < tol
-        fprintf('Converged in %d iterations.\n', k);
-        break;
-    end
-    
-    X = X_new;
+test_numerical_jacobian();
+disp('Tests Passed')
+
+% ------------------ TESTING MULTI_NEWTON_SOLVER -------------------------
+Xguess = rand(3, 1);
+
+solver_params = struct();
+solver_params.dxmin = 1e-10;
+solver_params.ftol = 1e-10;
+solver_params.dxmax = 1e8;
+solver_params.max_iters = 200;
+solver_params.numerical_diff = 0;
+
+X_root_analytical = multi_newton_solver(@test_function01, Xguess, solver_params);
+f_root_analytical = test_function01(X_root_analytical);
+disp(norm(f_root_analytical))
+
+solver_params.numerical_diff = 1;
+X_root_numerical = multi_newton_solver(@test_function01, Xguess, solver_params);
+f_root_numerical = test_function01(X_root_numerical);
+disp(norm(f_root_numerical))
+
+
+% ------------------ FINDING ROOT OF TEST_FUNCTION02 ---------------------
+
+X_root_02 = multi_newton_solver(@test_function02, Xguess, solver_params);
+f_root_02 = test_function02(X_root_02);
+disp(norm(f_root_02))
+
+
+% ------------------- ANIMATION COLLISION TESTING ------------------------
+
+solver_params.numerical_diff = 0;
+x_guess =  [0.52; 3];
+
+[x, exit_flag] = multi_newton_solver(@projectile_target_traj, x_guess, solver_params);
+
+if exit_flag
+    run_simulation(x(1), x(2))
 end
-
-disp('Root found:');
-disp(X);
-disp('Function evaluated at root:');
-disp(test_function01(X));
---
+++ projectile_target_traj.m
@@ -0,0 +1,3 @@
+function fun = projectile_target_traj(x)
+    fun = projectile_traj(x(1), x(2)) - target_traj(x(2));
+end
